464c562c936e9800c2a9ff83cfe62deed6a68048
cerdelen <cedric.erdelen@gmail.com>
2025-03-08 11:20:22 +0100

    Initial Commit

---
+++ dotaTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dotaTracker
+//
+//  Created by cerdelen on 08.03.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dotaTracker/dotaTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  dotaTrackerApp.swift
+//  dotaTracker
+//
+//  Created by cerdelen on 08.03.25.
+//
+
+import SwiftUI
+
+@main
+struct dotaTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
